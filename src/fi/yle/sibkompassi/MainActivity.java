66d5ceb63b8c06be5c73d7cfce7005f97b51e1b1
Emilia Mattila <emilia.mattila@eficode.com>
2015-07-16 11:19:13 +0300

    Added functionality to rotate the compass and upadate the heading in degrees.

--- src/fi/yle/sibkompassi/MainActivity.java
+++ src/fi/yle/sibkompassi/MainActivity.java
@@ -1,47 +1,71 @@
 package fi.yle.sibkompassi;
 
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
-    public final static String EXTRA_SONG = "fi.yle.sibkompassi.SONG";
-    ImageButton imgButton;
-    
+/**
+ * Calculations for compass heading by William J. Francis August 8, 2014
+ *
+ */
+public class MainActivity extends ActionBarActivity implements
+		SensorEventListener {
+	public final static String EXTRA_SONG = "fi.yle.sibkompassi.SONG";
+	ImageButton imgButton;
+	private SensorManager sensorManager;
+	private TextView heading;
+	private ImageView compass;
+	private float currentDegree = 0f;
+	private Sensor accelerometer;
+	private Sensor magnetometer;
+	private float[] mLastAccelerometer = new float[3];
+	private float[] mLastMagnetometer = new float[3];
+	private boolean mLastAccelerometerSet = false;
+	private boolean mLastMagnetometerSet = false;
+	private float[] mR = new float[9];
+	private float[] mOrientation = new float[3];
+
 	public void playSong(View view) {
 		Intent intent = new Intent(this, PlaySongActivity.class);
-		  TextView songNr = (TextView) findViewById(R.id.symphony_nr);
-		  String song = songNr.getText().toString();
-		  intent.putExtra(EXTRA_SONG, song);
+
+		TextView songNr = (TextView) findViewById(R.id.symphony_nr);
+		String song = songNr.getText().toString();
+		intent.putExtra(EXTRA_SONG, song);
 		// Verify that the intent will resolve to an activity
-		  if (intent.resolveActivity(getPackageManager()) != null) {
-		      startActivity(intent);
-		      Log.i("MainActivity", "MainActivity — intent started");
-		  }
-		  Log.i("MainActivity", "MainActivity — get song nr " + song);
+		if (intent.resolveActivity(getPackageManager()) != null) {
+			startActivity(intent);
+		}
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		View decorView = getWindow().getDecorView();
-		// Hide the status bar.
-		int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-		decorView.setSystemUiVisibility(uiOptions);
-		// Remember that you should never show the action bar if the
-		// status bar is hidden, so hide that too if necessary.
-		ActionBar actionBar = getSupportActionBar();
-		actionBar.hide();
+		hideStatusBar();
 		setContentView(R.layout.activity_main);
+		// initialize your android device sensor capabilities
+		heading = (TextView) findViewById(R.id.heading);
+		// initialize device sensor to detect changes in heading
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		accelerometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		magnetometer = sensorManager
+				.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		compass = (ImageView) findViewById(R.id.compass);
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,6 +85,75 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// no need
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+
+		if (event.sensor == accelerometer) {
+			System.arraycopy(event.values, 0, mLastAccelerometer, 0,
+					event.values.length);
+			mLastAccelerometerSet = true;
+		} else if (event.sensor == magnetometer) {
+			System.arraycopy(event.values, 0, mLastMagnetometer, 0,
+					event.values.length);
+			mLastMagnetometerSet = true;
+		}
+		if (mLastAccelerometerSet && mLastMagnetometerSet) {
+			SensorManager.getRotationMatrix(mR, null, mLastAccelerometer,
+					mLastMagnetometer);
+			SensorManager.getOrientation(mR, mOrientation);
+			float azimuthInRadians = mOrientation[0];
+			float degree = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+
+			// use displayDegree for displaying integer value of heading
+			int displayDegree = Math.round(degree);
+			heading.setText(Integer.toString(displayDegree) + " °");
+
+			RotateAnimation animation = new RotateAnimation(currentDegree,
+					-degree, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animation.setDuration(120);
+			animation.setFillAfter(true);
+			compass.startAnimation(animation);
+			currentDegree = -degree;
+		}
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// to save battery
+		// Register this class as a listener for the accelerometer sensor
+		sensorManager.registerListener(this, accelerometer,
+				SensorManager.SENSOR_DELAY_GAME);
+		// ...and the orientation sensor
+		sensorManager.registerListener(this, magnetometer,
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		// to save battery
+		sensorManager.unregisterListener(this, accelerometer);
+		sensorManager.unregisterListener(this, magnetometer);
+	}
+
+	private void hideStatusBar() {
+		View decorView = getWindow().getDecorView();
+		// Hide the status bar.
+		int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+		decorView.setSystemUiVisibility(uiOptions);
+		// Remember that you should never show the action bar if the
+		// status bar is hidden, so hide that too if necessary.
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.hide();
+	}
 
 }
