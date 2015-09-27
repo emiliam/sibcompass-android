a1c981502ec1464891ff8194a9afd40e7639037b
Emilia Mattila <emilia.mattila@eficode.com>
2015-09-27 18:31:45 +0300

    Changed back Yles license key

--- app/src/main/java/fi/yle/sibkompassi/extension/DownloaderService.java
+++ app/src/main/java/fi/yle/sibkompassi/extension/DownloaderService.java
@@ -1,7 +1,7 @@
 package fi.yle.sibkompassi.extension;
 
 public class DownloaderService extends com.google.android.vending.expansion.downloader.impl.DownloaderService {  
-    public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvLgeR6ItZ2FLUEZ9DHl5IGgD5H14kjs3HDo9TjO0+ks8i8eMeIFuhenx2g4xy8HCSdGL5ZF4CMaVHEywfDNhPZ138w4gGAPrkSrgzHJJqTSKIzmJ/HB0RzPSsKeD4N5yAbQf5FTvKCQbOsH6uYzXEcll6AbJ7ROnjlqYU1wcSpxThVgsHpXcH4FMXEcNttelc4m1FEkdP3e49HdCzEZUDlpr0iVdQid++sAMEiBsc6rK6BymVGaNCKWc9phG+FWrPmU7CJJZxEZj+mUqJDTlJWdkqR8b6zO5sSLa7Hu9uUbs4VaAfOci7yEZ6SRGtkgbBAKjvefhaAXrSuzX5z/AuQIDAQAB";
+    public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAj6MomLkhtH1J7cuh3VfqdrCS3BIFmvS/UESekdlI5osGxXh8BwquSPu0F2GbScg9VdWgj+VP6VR+6E9QjvlmQNjz99HUwsXhhsn2lduPAVkN0/WUDXVYhbcOWePF9BECbzAUcWMVgrf7DHDuHHoNvO8chasDvY7Rl2mLCWubYbUTB9AakrcJFTS9HcCNX1Hiqhw9QTZn8XrMPW64hvhztEyMP9wTy2wCWgsySbhY4BuSkrn5HOuBKIOl8OUsiBC8Mrm1GBkYWm/JAjOsHQtw/lMTo8R0zm0SEqmk9c62sufjX62yo29tNX23mLHHY125blJN2yy00+1DvFn8zYlExwIDAQAB";
     private static final byte[] SALT = new byte[]{64, -2, 33, 20,-14,-17,-29,10,-17,92,24,-23,-20,24,14,-53,22,-44,-27,16};
 
     @Override
